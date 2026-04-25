b0f41b66d0124dd3be7656bb9866e529f4f887ad
Jaime Alvarez <jaimeandresalvarez@gmail.com>
2026-04-25 02:10:17 -0500

    End of section 4

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using System;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,18 +10,20 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductsController(IProductRepository repo) : ControllerBase
+public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
 {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
     {
-        return Ok(await repo.GetProductsAsync(brand, type, sort));
+        var spec = new ProductSpecification(brand, type, sort);
+        var products = await repo.ListAllAsyncWithSpec(spec);
+        return Ok(products);
     }
 
     [HttpGet("{id:int}")] // api/products/2
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var product = await repo.GetProductByIdAsync(id);
+        var product = await repo.GetByIdAsync(id);
         if (product == null) return NotFound();
         return product;
     }
@@ -28,8 +31,8 @@ public class ProductsController(IProductRepository repo) : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product)
     {
-        repo.AddProduct(product);
-        if (await repo.SaveChangesAsync())
+        repo.Add(product);
+        if (await repo.SaveAllAsync())
         {
             return CreatedAtAction("GetProduct", new {id = product.Id}, product);
         }
@@ -42,9 +45,9 @@ public class ProductsController(IProductRepository repo) : ControllerBase
         if (product.Id != id || !ProductExists(id))
             return BadRequest("Cannot update this product");
 
-        repo.UpdateProduct(product);
+        repo.Update(product);
 
-        if (await repo.SaveChangesAsync())
+        if (await repo.SaveAllAsync())
         {
             return NoContent();
         }
@@ -55,14 +58,10 @@ public class ProductsController(IProductRepository repo) : ControllerBase
     [HttpDelete("{id:int}")]
     public async Task<ActionResult> DeleteProduct(int id)
     {
-        // var product = await context.Products.FindAsync(id);
-        var product = await repo.GetProductByIdAsync(id);
+        var product = await repo.GetByIdAsync(id);
         if (product == null) return NotFound();
-        // context.Products.Remove(product);
-        repo.DeleteProduct(product);
-        // await context.SaveChangesAsync();
-        // return NoContent();
-        if (await repo.SaveChangesAsync())
+        repo.Remove(product);
+        if (await repo.SaveAllAsync())
         {
             return NoContent();
         }
@@ -72,17 +71,19 @@ public class ProductsController(IProductRepository repo) : ControllerBase
     [HttpGet("brands")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
     {
-        return Ok(await repo.GetBrandsAsync());
+        var spec = new BrandListSpecification();
+        return Ok(await repo.ListAllAsyncWithSpec(spec));
     }
 
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
     {
-        return Ok(await repo.GetTypesAsync());
+        var spec = new TypeListSpecification();
+        return Ok(await repo.ListAllAsyncWithSpec(spec));
     }
 
     private bool ProductExists(int id)
     {
-        return repo.ProductExists(id);
+        return repo.Exists(id);
     }
 }
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Runtime.CompilerServices;
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T:BaseEntity
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T?> GetWithSpec(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> ListAllAsyncWithSpec(ISpecification<T> spec);
+    Task<TResult?> GetWithSpec<TResult>(ISpecification<T, TResult> spec);
+    Task<IReadOnlyList<TResult>> ListAllAsyncWithSpec<TResult>(ISpecification<T, TResult> spec);
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<bool> SaveAllAsync();
+    bool Exists(int id);
+}
